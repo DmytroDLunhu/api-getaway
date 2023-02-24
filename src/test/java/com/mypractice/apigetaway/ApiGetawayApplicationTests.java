61eff5a257f47d7b483f5ffab72a3178dd1cc9af
Dmytro Lunhu <dlunhu@gmail.com>
2023-02-24 10:56:09 -0500

    First stage. Sceleton of project

---
+++ src/test/java/com/mypractice/apigetaway/ApiGetawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypractice.apigetaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGetawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
